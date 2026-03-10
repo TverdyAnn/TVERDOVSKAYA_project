2a53b12f4a001cba8bd1b84d16c872c57d70b087
TverdyAnn <annn.tverdy@gmail.com>
2026-03-10 12:59:34 +0300

    Create task1.py

---
+++ task1/task1.py
@@ -0,0 +1,16 @@
+
+student_name = "Синиких А. А., Твердовская А. И., Волохин К. Н."
+group_number = "3140801/520502"
+project_name = "ЖК Наука"
+floors = 16
+height = 27.0
+is_residential = True
+construction_year = 2023
+print("=== ПАСПОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА ===")
+print(f"Объект: {project_name}")
+print(f"Этажность: {floors} этажей")
+print(f"Высота: {height} м")
+print(f"Тип: Жилой" if is_residential == True else f"Тип: Нежилой")
+print(f"Год постройки: {construction_year}")
+# ул. Науки
+# находится недалеко от м. Политехническая и м. Академическая
